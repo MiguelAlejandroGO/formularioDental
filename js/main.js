12c8034149d581acc7f5a9e5be146bafa3915aef
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-12-05 13:34:52 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,6 +45,19 @@ window.addEventListener("offline", () => {
 });
 
 function addDate() {
+  let dataForm = {
+    id: 0,
+    name: document.querySelector("#name").value,
+    age: parseInt(document.querySelector("#age").value),
+    address: document.querySelector("#address").value,
+    numberTel: document.querySelector("#numberTel").value,
+    numberCel: document.querySelector("#numberCel").value,
+    email: document.querySelector("#email").value,
+    dateLast: document.querySelector("#datePast").value,
+    dateNew: document.querySelector("#dateNew").value,
+    hoursDate: document.querySelector("#timeNew").value,
+    message: document.querySelector("#message").value,
+  };
 
  
   fetch('https://api-citas-dental.vercel.app/create/', {
@@ -53,37 +66,14 @@ function addDate() {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-      id: 0,
-      name: "Miguel Gutierrez",
-      age: 22,
-      address: "Ebanos #105",
-      numberTel: "7641257458",
-      numberCel: "7641025856",
-      email: "migue@gmail.com",
-      dateLast: "2021-12-04",
-      dateNew: "2021-12-05",
-      hoursDate: "13:25",
-      message: "Dolor de muelas"})
+      body: JSON.stringify(dataForm)
     }) .then((response) => response.json())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));;
    
   
 
-  // let dataForm = {
-  //   id: 0,
-  //   name: document.querySelector("#name").value,
-  //   age: parseInt(document.querySelector("#age").value),
-  //   address: document.querySelector("#address").value,
-  //   numberTel: document.querySelector("#numberTel").value,
-  //   numberCel: document.querySelector("#numberCel").value,
-  //   email: document.querySelector("#email").value,
-  //   dateLast: document.querySelector("#datePast").value,
-  //   dateNew: document.querySelector("#dateNew").value,
-  //   hoursDate: document.querySelector("#timeNew").value,
-  //   message: document.querySelector("#message").value,
-  // };
+
 
   // var requestOptions = {
   //   method: "POST",
