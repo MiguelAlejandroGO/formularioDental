d7791d0bbf18b3122705489411bf3611b327e671
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-12-05 13:13:11 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,31 +46,56 @@ window.addEventListener("offline", () => {
 
 function addDate() {
 
-  let dataForm = {
-    id: 0,
-    name: document.querySelector("#name").value,
-    age: parseInt(document.querySelector("#age").value),
-    address: document.querySelector("#address").value,
-    numberTel: document.querySelector("#numberTel").value,
-    numberCel: document.querySelector("#numberCel").value,
-    email: document.querySelector("#email").value,
-    dateLast: document.querySelector("#datePast").value,
-    dateNew: document.querySelector("#dateNew").value,
-    hoursDate: document.querySelector("#timeNew").value,
-    message: document.querySelector("#message").value,
-  };
+  (async () => {
+    const rawResponse = await fetch('https://api-citas-dental.vercel.app/create/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+      id: 0,
+      name: "Miguel Gutierrez",
+      age: 22,
+      address: "Ebanos #105",
+      numberTel: "7641257458",
+      numberCel: "7641025856",
+      email: "migue@gmail.com",
+      dateLast: "2021-12-04",
+      dateNew: "2021-12-05",
+      hoursDate: "13:25",
+      message: "Dolor de muelas"})
+    });
+    const content = await rawResponse.json();
+  
+    console.log(content);
+  })();
 
-  var requestOptions = {
-    method: "POST",
-    headers: new Headers({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    }),
-    body: JSON.stringify(dataForm),
-    mode: "no-cors",
-  };
-  fetch("https://api-citas-dental.vercel.app/create", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
+  // let dataForm = {
+  //   id: 0,
+  //   name: document.querySelector("#name").value,
+  //   age: parseInt(document.querySelector("#age").value),
+  //   address: document.querySelector("#address").value,
+  //   numberTel: document.querySelector("#numberTel").value,
+  //   numberCel: document.querySelector("#numberCel").value,
+  //   email: document.querySelector("#email").value,
+  //   dateLast: document.querySelector("#datePast").value,
+  //   dateNew: document.querySelector("#dateNew").value,
+  //   hoursDate: document.querySelector("#timeNew").value,
+  //   message: document.querySelector("#message").value,
+  // };
+
+  // var requestOptions = {
+  //   method: "POST",
+  //   headers: new Headers({
+  //     "Content-Type": "application/json",
+  //     "Access-Control-Allow-Origin": "*",
+  //   }),
+  //   body: JSON.stringify(dataForm),
+  //   mode: "no-cors",
+  // };
+  // fetch("https://api-citas-dental.vercel.app/create", requestOptions)
+  //   .then((response) => response.text())
+  //   .then((result) => console.log(result))
+    //.catch((error) => console.log("error", error));
 }
